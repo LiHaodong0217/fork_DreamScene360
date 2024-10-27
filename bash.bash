4a0b850b45278532d33bac209e4188ebe500e056
LiHaodong0217 <3196859347@qq.com>
2024-10-27 09:50:11 +0800

    whole pipeline experiment

--- bash.bash
+++ bash.bash
@@ -5,3 +5,12 @@ python train.py -s data/YOUR_SCENE -m output/OUTPUT_NAME
 python train.py -s /public/home/cit_haodongli/DreamScene360/data/alley_pano/ -m output/PeRF_kitchen
 #render
 python render.py -s data/YOUR_SCENE -m output/OUTPUT_NAME  --iteration 9000
+
+#测试有全部监督
+python train.py -s /public/home/cit_haodongli/DreamScene360/data/alley_pano/ -m output/PeRF_kitchen
+
+python train.py -s /public/home/cit_haodongli/DreamScene360/data/alley_pano/ -m output/PeRF_kitchen_without_depth
+#测试没有depth监督
+python train_without_depth.py -s /public/home/cit_haodongli/DreamScene360/data/alley_pano/ -m output/PeRF_kitchen_without_depth
+#测试没有segment监督
+python train_without_segment.py -s /public/home/cit_haodongli/DreamScene360/data/alley_pano/ -m output/PeRF_kitchen_without_segment
